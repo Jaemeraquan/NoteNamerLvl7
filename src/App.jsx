7e8c6e49b80152fa714ab70c3e2e0bca32364d8f
Jaemeraquan <138228919+Jaemeraquan@users.noreply.github.com>
2023-10-09 10:41:48 -0400

    update build for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -234,12 +234,12 @@ useEffect(() => {
   setAskedQuestions([]);
 }, []);
 
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
+function shuffleArray(array) {
+  for (let i = 1; i < array.length; i++) {
+    const j = Math.floor(Math.random() * (array.length - i)) + 1;
+    [array[i], array[j]] = [array[j], array[i]];
   }
+}
   // Define a state variable to track the button text
   const [viewScoreText, setViewScoreText] = useState('View Score');
 
